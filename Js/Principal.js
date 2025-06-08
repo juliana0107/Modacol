741bfdb4ee70b5f440428ccfd2dabea77719f9de
Your Name <you@example.com>
2025-06-07 19:03:51 -0500

    Primer commit

---
+++ Js/Principal.js
@@ -0,0 +1,83 @@
+/* Roles */
+
+function Roles() {
+      document.getElementById("Roles").style.display = "block";
+    }
+
+function CerrarRoles() {
+      document.getElementById("Roles").style.display = "none";
+    }
+
+
+/* Ventas */
+function abrirVentas() {
+      document.getElementById("Ventas").style.display = "block";
+    }
+
+
+    function CerrarVentas() {
+      document.getElementById("Ventas").style.display = "none";
+    }
+
+/* Productos */
+
+function abrirProductos() {
+      document.getElementById("Productos").style.display = "block";
+    }
+
+
+    function CerrarProductos() {
+      document.getElementById("Productos").style.display = "none";
+    }
+
+/* Proveedores */
+
+function abrirProveedores() {
+      document.getElementById("Proveedores").style.display = "block";
+    }
+
+
+    function CerrarProveedores() {
+      document.getElementById("Proveedores").style.display = "none";
+    }
+
+/* Clientes */
+
+function abrirClientes() {
+      document.getElementById("Clientes").style.display = "block";
+    }
+
+
+    function CerrarClientes() {
+      document.getElementById("Clientes").style.display = "none";
+    }
+
+/* Compras */
+function abrirCompras() {
+      document.getElementById("Compras").style.display = "block";
+    }
+
+
+    function CerrarCompras() {
+      document.getElementById("Compras").style.display = "none";
+    }
+
+/* Categorias */
+function abrirCategorias() {
+      document.getElementById("Categorias").style.display = "block";
+    }
+
+
+    function CerrarCategorias() {
+      document.getElementById("Categorias").style.display = "none";
+    }
+
+/* Flujo */
+function abrirFlujo() {
+      document.getElementById("Flujo").style.display = "block";
+    }
+
+
+    function CerrarFlujo() {
+      document.getElementById("Flujo").style.display = "none";
+    }
